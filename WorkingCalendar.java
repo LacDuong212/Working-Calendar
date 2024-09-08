8bbc4c372b7aa8be669a71fbd99307157311bd96
LDuong <lacduongldg212@gmail.com>
2024-09-08 16:07:50 +0700

    Add computer information.

--- WorkingCalendar.java
+++ WorkingCalendar.java
@@ -1,5 +1,8 @@
 public class WorkingCalendar{
     public static void main(String[] args) {
         System.out.println("Welcome the Working Calendar project.");
+        System.out.println("Operating System: " + System.getProperty("os.name"));
+        System.out.println("Operating System Version: " + System.getProperty("os.version"));
+        System.out.println("CPU Architecture: " + System.getProperty("os.arch"));
     }
 }
